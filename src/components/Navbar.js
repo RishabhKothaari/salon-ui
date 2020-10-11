2eacc5d5392d7436aab784f6eb9036c39e2ac3e0
rishab <rishab.inder@gmail.com>
2020-10-11 13:21:59 -0700

    salon-ui 🎉 inital setup 🚀

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navbar() {
+  /*
+    <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">
+          AR Salon & Spa
+        </a>
+      </nav>
+    */
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">
+        AR Salon & Spa
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
